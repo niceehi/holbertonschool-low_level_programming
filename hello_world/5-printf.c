ca1c66da5377973259b31f7a23e68d4af6f8a815
niceehi <hsnislam2026@gmail.com>
2025-12-01 00:14:57 +0400

    Update main function to print a new message

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - Prints the size of various types based on
- *        the computer it is compiled and run on..
+ * main - Prints "with proper grammar, but the outcome is
+ *                a piece of art," followed by a new line.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	printf("Size of a char: %zu byte(s)\n", sizeof(char));
-	printf("Size of an int: %zu byte(s)\n", sizeof(int));
-	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
 	return (0);
 }
