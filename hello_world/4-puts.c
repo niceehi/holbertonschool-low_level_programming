c47b3cfd2a8fc4e56892dfac38031bb9b9197f46
niceehi <hsnislam2026@gmail.com>
2025-12-01 01:08:00 +0400

    Correct comment formatting and include statement
    Fix formatting and add missing new line in comment.

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,7 @@
-
-  #include <stdio.h>
+#include <stdio.h>
+/** * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+*/
 
 
 int main(void)
