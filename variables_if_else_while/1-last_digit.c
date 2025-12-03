b2b8bf8f01750467f9fd308cbd2bf72b90fa9512
niceehi <hsnislam2026@gmail.com>
2025-12-03 12:42:00 +0400

    Refactor last digit checks into else-if structure

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdio.h>
 
-/* * 
+/**
  * main - print last digit in random number
  *
  * Description: print last digit in random number
@@ -12,24 +12,25 @@
 int main(void)
 {
 	int n;
-    int last;
-    
-    srand(time(0));
-	n = rand() - RAND_MAX / 2;
-    last = n % 10;
+	int last;
 
-	if(last > 5)
-    {
-        printf("Last digit of %d is %d and is greater than 5\n", n, last);
-    }
-    if(last == 0)
-    {
-        printf("Last digit of %d is %d and is 0\n", n, last);
-    }
-    if(last!=0 && last < 6)
-    {
-        printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
-    }
-    
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	last = n % 10;
+
+	if (last > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, last);
+	}
+	else if (last == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, last);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n",
+			n, last);
+	}
+
 	return (0);
 }
