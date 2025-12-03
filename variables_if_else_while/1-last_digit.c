dfd3e39932a738eb72aa478218a82a0149285db1
niceehi <hsnislam2026@gmail.com>
2025-12-03 12:39:30 +0400

    Update main function documentation and comments

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -3,10 +3,11 @@
 #include <stdio.h>
 
 /* * 
-*main - Prints a text according number
+* main - print last digit in random number
  *
- * Return: Always (Success)
+ * Description: print last digit in random number
  *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
