c0b56ddd49fd66b614cdd2de48a87230b5a0a1fd
niceehi <hsnislam2026@gmail.com>
2025-12-03 11:43:20 +0400

    Refactor main function to check for zero explicitly

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -3,30 +3,29 @@
 #include <stdio.h>
 
 /**
-*main - function checks random rumber positive or zero
-*
-*Return: Always 0.
-*/
+ * main - Prints if number is positive, zero or negative
+ *
+ * Return: Always (Success)
+ */
 int main(void)
 {
-  
-  int n;
-  srand(time(0));
-  n = rand() - RAND_MAX / 2;
+	int n;
 
-  if (n > 0)
-  {  
-      printf("%d is positive\n", n);
-  }
-  else if (n < 0)
-  {
-      printf("%d is negative\n", n);
-  }  
-  else
-  {
-      printf("%d is zero\n", n);
-  }
-  
-  return (0);
-  
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+
+	return (0);
 }
