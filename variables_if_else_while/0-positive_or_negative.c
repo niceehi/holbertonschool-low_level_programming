5221c85121c9e4e749b25498cd80589cecf7abb5
niceehi <hsnislam2026@gmail.com>
2025-12-03 11:41:20 +0400

    Refactor positive or negative check for clarity

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -7,30 +7,28 @@
 *
 *Return: Always 0.
 */
-
 int main(void)
 {
   
-int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
+  int n;
+  srand(time(0));
+  n = rand() - RAND_MAX / 2;
 
-if (n > 0)
-{
-printf("%d is positive\n", n);
-}
+  if (n > 0)
+  {  
+  printf("%d is positive\n", n);
+  }
   
-else if (n < 0)
-{
-printf("%d is negative\n", n);
-}
+  else if (n < 0)
+  {
+  printf("%d is negative\n", n);
+  }  
   
-else
-{
-printf("%d is zero\n", n);
-}
+  else
+  {
+  printf("%d is zero\n", n);
+  }
   
-return (0);
+  return (0);
   
 }
-
