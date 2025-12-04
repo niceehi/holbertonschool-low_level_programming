a63f4b7d91fa49b7915e517fe60b56ef37675e5f
niceehi <hsnislam2026@gmail.com>
2025-12-04 22:13:22 +0400

    Update 5-print_numbers.c

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,19 +1,13 @@
 #include <stdio.h>
 
-/**
-  * main - Prints the numbers since 0 to 9
-  *
-  * Return: Always (Success)
-  */
 int main(void)
 {
     char i;
-	for (i='0'; i <= '9'; i++)
-    {
-		putchar(i);
-    }
 
-	putchar('\n');
+    for (i = '0'; i <= '9'; i++)
+        putchar(i);
 
-	return (0);
+    putchar('\n');
+
+    return (0);
 }
