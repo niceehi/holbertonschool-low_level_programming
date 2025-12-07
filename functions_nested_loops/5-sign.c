ac4cda97f10849f99549e8970c857df04b7a3a02
niceehi <hsnislam2026@gmail.com>
2025-12-07 23:01:11 +0400

    Tasks 5

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ * @n: number to check
+ *
+ * Return: 1 if positive, 0 if zero, -1 if negative
+ */
+int print_sign(int n)
+{
+    if(n>0)
+    {
+        return(1);
+        _putchar('+');
+    }
+    else if(n<0)
+    {
+        return(-1);
+        _putchar('-');
+    }
+    else if(n==0)
+    {
+        return(0);
+        _putchar('0');
+    }
+}
