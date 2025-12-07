b1e0cc1e289dd07d6e620036467bdf95d7fd06eb
niceehi <hsnislam2026@gmail.com>
2025-12-07 23:30:19 +0400

    Tasks 6

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -8,12 +8,12 @@
  */
 int _abs(int n)
 {
-    if(n < 0)
-    {
-        return(-n);
-    }
-    else
-    {
-        return(n);
-    }
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
 }
