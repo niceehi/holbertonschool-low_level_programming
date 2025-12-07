a1eda4e2eca495bfc22f39ea0dc6bc73d84044de
niceehi <hsnislam2026@gmail.com>
2025-12-07 21:51:04 +0400

    Fix reset of x in print_alphabet_x10 function
    Reset x to 'a' for each iteration of the outer loop.

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -10,15 +10,16 @@ void print_alphabet_x10(void)
 	int i = 0;
 	char x;
 
-	while (i < 10) 
+	while (i < 10)
 	{
-		x = 'a'; 
+		x = 'a'; // Сброс x на 'a' для каждой итерации
 		
-		while (x <= 'z') 
+		while (x <= 'z')
 		{
 			_putchar(x);
 			x++;
 		}
+		
 		_putchar('\n');
 		i++;
 	}
