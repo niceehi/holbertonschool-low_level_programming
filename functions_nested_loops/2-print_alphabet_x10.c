22b96e46f70cc6060dc64537e986067aac132639
niceehi <hsnislam2026@gmail.com>
2025-12-07 21:47:40 +0400

    Tasks 2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet in lowercase 10x
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char x;
+
+	while (i < 10) 
+	{
+		x = 'a'; 
+		
+		while (x <= 'z') 
+		{
+			_putchar(x);
+			x++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
