81bfd438771f10730e06189d842de91784f06b01
niceehi <hsnislam2026@gmail.com>
2025-12-07 23:52:48 +0400

    Tasks 7

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -10,11 +10,11 @@ int print_last_digit(int n)
 {
     int x;
     x = n % 10;
-    if(n<0)
+    if(n < 0)
     {
         x = x * (-1);
     }
-    _putchar(x);
+    _putchar(x + '0');
 
     return(x);
 }
