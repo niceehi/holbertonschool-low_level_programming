5ab29281139f458ab54c42c502c237e75d7a046e
niceehi <hsnislam2026@gmail.com>
2025-12-10 12:54:31 +0400

    Tasks 0

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
-  * _isupper - Check if a letter is upper
-  * @x: The number to be checked
-  *
-  * Return: 1 for upper letter or 0 for any else
-  */
+ * _isupper - Check if a letter is upper
+ * @c: The character to be checked
+ *
+ * Return: 1 for upper letter or 0 for any else
+ */
 int _isupper(int c)
 {
-    if(c >= 65 && c<=90)
+    if (c >= 65 && c <= 90)
     {
-        return(1);
+        return (1);
     }
-    else 
+    else
     {
-        return(0);
+        return (0);
     }
 }
