6b34d1dfa3491f952f598f66d1df15c5c780f436
niceehi <justemail648777653@gmail.com>
2025-12-11 13:58:07 +0400

    Tasks 5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -12,15 +12,12 @@ void more_numbers(void)
     {
         for(i = 0; i <= 14; i++)
         {
-            if (i < 10)
+            if (i >10)
             {
-                _putchar(i + '0');
-            }
-            else
-            {
+
                 _putchar((i / 10) + '0');
-                _putchar((i % 10) + '0');
             }
+            _putchar((i % 10) + '0');
         }
         _putchar('\n');
         k++;
