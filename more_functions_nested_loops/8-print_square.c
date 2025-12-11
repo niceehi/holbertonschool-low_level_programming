a41a969907551dad7787d6c6d346c2147b58125f
niceehi <hsnislam2026@gmail.com>
2025-12-12 01:52:45 +0400

    Tasks 8

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -10,13 +10,16 @@ void print_square(int size)
     int i;
     int k;
     k = size;
-    while(k > 0)
+    if(size<=0)
     {
-        for(i=0; i<size; i++)
+        while(k > 0)
         {
-            _putchar(35);
+            for(i=0; i<size; i++)
+            {
+                _putchar(35);
+            }
+            _putchar('\n');
+            k = k - 1;
         }
-        _putchar('\n');
-        k = k - 1;
     }
 }
