e35cc8b163756de29143fd365ebfead31d97885c
niceehi <justemail648777653@gmail.com>
2025-12-11 13:29:52 +0400

    Tasks 5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers - Prints the numbers 0-14 ten times.
+ */
+void more_numbers(void)
+{
+    int k = 0;
+
+    while (k <= 9)
+    {
+        for (int i = 0; i <= 14; i++)
+        {
+            if (i < 10)
+            {
+                _putchar(i + '0');
+            }
+            else
+            {
+                _putchar((i / 10) + '0');
+                _putchar((i % 10) + '0');
+            }
+        }
+        _putchar('\n');
+        k++;
+    }
+}
