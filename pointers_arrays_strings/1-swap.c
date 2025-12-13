fddcd9ee7c879001855942029fa5342b1395dcdc
niceehi <hsnislam2026@gmail.com>
2025-12-14 03:33:15 +0400

    Tasks 1

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,4 +1,10 @@
 #include "main.h"
+
+/**
+ * swap_int - Swaps the values of two int.
+ * @a: The first int to be swapped.
+ * @b: The second int to be swapped.
+ */
 void swap_int(int *a, int *b)
 {
     int t = *a;
