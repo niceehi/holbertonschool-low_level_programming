adc04411de862529f9f5b3d4329530ef1a53de3a
niceehi <hsnislam2026@gmail.com>
2025-12-14 20:15:50 +0400

    Task 4

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -7,11 +7,12 @@
 void print_rev(char *s)
 {
     int i, k;
+    k = 0;
     for(i=0; s[i] != '\0'; i++)
     {
         k = k+1;
     }
-    while(k>0)
+    while(k>=0)
     {
         _putchar(s[k]);
         k= k - 1;
