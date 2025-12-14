37546cddfd42f547fff74504517cf984d5a7582a
niceehi <hsnislam2026@gmail.com>
2025-12-14 19:15:35 +0400

    Tasks 2

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * _strlen - Returns the length of a string.
- * @str: The string to get the length of.
+ * _strlen - Returns the length of a string
+ * @str: The string to get the length of
  *
- * Return: The length of @str.
+ * Return: The length of @str
  */
 int _strlen(char *s)
 {
