de47b07aa1c18be6a2208f1e31b77c233541ed1b
niceehi <hsnislam2026@gmail.com>
2025-12-15 03:14:22 +0400

    Task 9

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _atoi - Converts a string to an integer.
+ * @s: The string to be converted.
+ *
+ * Return: The integer value of the converted string.
+ */
+int _atoi(char *s)
+{
+    int i = 0;
+    int sign = 1;
+    int r = 0;
+    int st = 0;
+
+    while (s[i])
+    {
+        if (s[i] == '-')
+            sign *= -1;
+        else if (s[i] == '+')
+            ;
+        else if (s[i] >= '0' && s[i] <= '9')
+        {
+            st = 1;
+            r = r * 10 + (s[i] - '0');
+        }
+        else if (st)
+            break;
+
+        i++;
+    }
+
+    return r * sign;
+}
