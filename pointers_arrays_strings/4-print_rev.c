b69bc90941d8e0035c9dd2939a565f9347c1facb
niceehi <hsnislam2026@gmail.com>
2025-12-14 20:13:20 +0400

    Task 4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+void print_rev(char *s)
+{
+    int i, k;
+    for(i=0; s[i] != '\0'; i++)
+    {
+        k = k+1;
+    }
+    while(k>0)
+    {
+        _putchar(s[k]);
+    }
+    _putchar('\n');
+}
