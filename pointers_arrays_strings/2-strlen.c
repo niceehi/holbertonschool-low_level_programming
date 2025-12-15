f3928def9ec105da8c3ef4f72778274180ca19f7
niceehi <hsnislam2026@gmail.com>
2025-12-15 23:39:40 +0400

    Task 2

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -2,16 +2,18 @@
 
 /**
  * _strlen - Returns the length of a string
- * @str: The string to get the length of
+ * @s: The string to get the length of
  *
- * Return: The length of @str
+ * Return: The length of @s
  */
 int _strlen(char *s)
 {
-    int n = 0;
-    while(s[n] != '\0')
-    {
-        n++;
-    }
-    return (n);
+	int n = 0;
+
+	while (s[n] != '\0')
+	{
+		n++;
+	}
+
+	return (n);
 }
