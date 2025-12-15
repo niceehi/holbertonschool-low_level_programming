f8b94ff502063da090dd070e64b2b36628da49a1
niceehi <hsnislam2026@gmail.com>
2025-12-16 02:04:15 +0400

    Task 1

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates at most n characters from src to dest
+ * @dest: destination string
+ * @src: source string
+ * @n: maximum number of characters to copy from src
+ *
+ * Return: pointer to dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+    int i, k = 0;
+
+    while (dest[k] != '\0')
+    {
+        k++;
+    }
+
+    for (i = 0; i < n && src[i] != '\0'; i++)
+    {
+        dest[k + i] = src[i];
+    }
+
+    dest[k + i] = '\0';
+
+    return dest;
+}
