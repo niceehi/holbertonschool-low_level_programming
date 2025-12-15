87fd6da83c3ce78b4813333e2b97c05a8afc0306
niceehi <hsnislam2026@gmail.com>
2025-12-15 23:43:17 +0400

    Task 6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -6,15 +6,20 @@
  */
 void puts2(char *str)
 {
-    int k=0, i;
-    
-    while(str[k] != '\0')
-    {
-        k++;
-    }
-    for(i=0; i < k; i=i+2)
-    {
-        _putchar(str[i]);
-    }
-    _putchar('\n');
+	int i;
+	int k;
+
+	k = 0;
+
+	while (str[k] != '\0')
+	{
+		k++;
+	}
+
+	for (i = 0; i < k; i += 2)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
 }
