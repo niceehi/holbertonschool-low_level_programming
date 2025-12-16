ae03810f39acaaa50ff08f1df0c7fb7b6335cf62
niceehi <justhsnmax1212@gmail.com>
2025-12-16 12:24:16 +0400

    Task 2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -2,24 +2,27 @@
 
 /**
  * _strncpy - Copies at most n characters from the string pointed to by src
- *            to the buffer pointed to by dest.
+ *            to the buffer pointed to by dest
  * 
- * @dest: Pointer to the destination buffer where the content is copied.
- * @src: Pointer to the source string that is copied.
- * @n: The maximum number of characters to be copied.
+ * @dest: Pointer to the destination buffer where the content is copied
+ * @src: Pointer to the source string that is copied
+ * @n: The maximum number of characters to be copied
+ * 
+ * Return: A pointer to the destination string dest
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-    int i;
+	int i;
 
-    for (i = 0; i < n && src[i] != '\0'; i++)
-    {
-        dest[i] = src[i];
-    }
-    for (; i < n; i++)
-    {
-        dest[i] = '\0';
-    }
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
 
-    return dest;
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+
+	return (dest);
 }
