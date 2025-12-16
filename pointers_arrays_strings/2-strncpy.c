327cee99a17524e3f6a131c6069b9453dd8dcd2a
niceehi <hsnislam2026@gmail.com>
2025-12-16 12:28:29 +0400

    Task 2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -3,11 +3,9 @@
 /**
  * _strncpy - Copies at most n characters from the string pointed to by src
  *            to the buffer pointed to by dest
- * 
  * @dest: Pointer to the destination buffer where the content is copied
  * @src: Pointer to the source string that is copied
  * @n: The maximum number of characters to be copied
- * 
  * Return: A pointer to the destination string dest
  */
 char *_strncpy(char *dest, char *src, int n)
