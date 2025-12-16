c5962e639ec7872785eafdf5ee93a8f01e00910f
niceehi <justhsnmax1212@gmail.com>
2025-12-16 11:51:04 +0400

    Task 2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -8,7 +8,7 @@ char *_strncpy(char *dest, char *src, int n)
     }
     for(i = 0; i<n && src[i] != '\0'; i++)
     {
-        dest[k+i] = src[i]
+        dest[k+i] = src[i];
     }
     dest[k + i] = '\0';
 
