aebf07c4f29768bfb78d193a7bab5e749f8de955
niceehi <justhsnmax1212@gmail.com>
2025-12-16 11:56:22 +0400

    Task 2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,16 +1,13 @@
 #include "main.h"
 char *_strncpy(char *dest, char *src, int n)
 {
-    int i, k=0;
-    while(src[k] != '\0')
-    {
-        k++;
-    }
+    int i;
+
     for(i = 0; i<n && src[i] != '\0'; i++)
     {
-        dest[k+i] = src[i];
+        dest[i] = src[i];
     }
-    dest[k + i] = '\0';
+    dest[i] = '\0';
 
     return(dest);
 }
