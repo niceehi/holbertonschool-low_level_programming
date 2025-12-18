e877aca585bea48ce6bc06b91addc9e6cef8df7f
niceehi <hsnislam2026@gmail.com>
2025-12-18 22:34:40 +0400

    Task 7

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -6,8 +6,7 @@
  */
 char *leet(char *s)
 {
-	int i;
-	int k;
+	int i, k;
 	char a[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
 	char b[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
 
