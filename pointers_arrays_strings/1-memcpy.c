26e6428fa0021f25d42ef432dd35b6dc186b70e0
niceehi <justmail2026isloknice@gmail.com>
2025-12-19 12:43:23 +0400

    Task 1

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area.
+ * @dest: pointer to the destination memory area.
+ * @src: pointer to the source memory area.
+ * 
+ * Return: pointer to the destination memory area dest.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
