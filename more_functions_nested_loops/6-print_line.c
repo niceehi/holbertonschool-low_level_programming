64876e417855bddffa0bad07cb2555ebec1b6c0c
niceehi <hsnislam2026@gmail.com>
2025-12-19 11:42:33 +0400

    Task 6

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -7,13 +7,13 @@
  */
 void print_line(int n)
 {
-    if(n>0)
-    {
-        while(n!=0)
-        {
-            _putchar(95);
-            n = n - 1;
-        }
-    }
-    _putchar('\n');
+	if (n > 0)
+	{
+		while (n != 0)
+		{
+			_putchar(95);
+			n = n - 1;
+		}
+	}
+	_putchar('\n');
 }
