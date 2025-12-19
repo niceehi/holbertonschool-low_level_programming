9e74fccf395c4cbc349d5daeaccbe0081f6e28ec
niceehi <hsnislam2026@gmail.com>
2025-12-19 11:38:10 +0400

    Task 5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -5,20 +5,20 @@
  */
 void more_numbers(void)
 {
-    int k = 0;
-    int i;
+	int k = 0;
+	int i;
 
-    while (k < 10)
-    {
-        for (i = 0; i <= 14; i++)
-        {
-            if (i >= 10)
-            {
-                _putchar((i / 10) + '0');
-            }
-            _putchar((i % 10) + '0');
-        }
-        _putchar('\n');
-        k++;
-    }
+	while (k < 10)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i >= 10)
+			{
+				_putchar((i / 10) + '0');
+			}
+			_putchar((i % 10) + '0');
+		}
+		_putchar('\n');
+		k++;
+	}
 }
