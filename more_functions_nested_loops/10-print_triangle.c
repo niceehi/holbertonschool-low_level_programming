5f58bd4fe18a0ed21bf6c4e1370aaede16c6b4d4
niceehi <justmail2026isloknice@gmail.com>
2025-12-23 13:17:26 +0400

    Task 10

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -20,13 +20,13 @@ void print_triangle(int size)
 		return;
 	}
 
-	for (i = 0; i < size; i++)
+	for (i = 1; i <= size; i++)
 	{
-		for (j = 0; j < i; j++)
+		for (j = 1; j <= size - i; j++)
 		{
 			_putchar(' ');
 		}
-		for (j = size - i; j > 0; j--)
+		for (j = 1; j <= i; j++)
 		{
 			_putchar('#');
 		}
