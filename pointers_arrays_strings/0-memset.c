12ece2092b65013476796efc0bd5a21a1ad1f518
niceehi <hsnislam2026@gmail.com>
2025-12-23 11:25:01 +0400

    Task 0

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -3,7 +3,8 @@
 /**
  * _memset - fills memory with a constant byte.
  * @s: pointer to the memory area to be filled.
- * @b: the constant byte to fill the memory with
+ * @b: the constant byte to fill the memory with.
+ *
  * Return: pointer to the memory area `s`.
  */
 char *_memset(char *s, char b, unsigned int n)
