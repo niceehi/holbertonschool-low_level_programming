3a4a3f20dc3448b3eb74d76517ffff7b5cf0e922
niceehi <hsnislam2026@gmail.com>
2025-12-23 11:50:59 +0400

    Task 8

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -8,19 +8,19 @@
  */
 void print_diagsums(int *a, int size)
 {
-    int i, j, x = 0, y = 0;
+	int i, j, x = 0, y = 0;
 
-    for (i = 0; i < size; i++)
-    {
-        for (j = 0; j < size; j++)
-        {
-            if (i == j)
-                x = x + a[i * size + j];
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			if (i == j)
+				x = x + a[i * size + j];
 
-            if (i + j == size - 1)
-                y = y + a[i * size + j];
-        }
-    }
+			if (i + j == size - 1)
+				y = y + a[i * size + j];
+		}
+	}
 
-    printf("%d, %d\n", x, y);
+	printf("%d, %d\n", x, y);
 }
