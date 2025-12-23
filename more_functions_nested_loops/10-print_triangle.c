98971177687c04bcebac8cdd6d59559adb2b2654
niceehi <justmail2026isloknice@gmail.com>
2025-12-23 13:14:26 +0400

    Task 10

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -1,32 +1,36 @@
 #include "main.h"
 
 /**
- * print_triangle - prints a triangle, followed by a new line.
+ * print_triangle - prints an inverted triangle, followed by a new line.
  * @size: The size of the triangle.
  * 
- * Description: This function prints a right-angled triangle of size `size` using the character `#`.
+ * Description: This function prints an inverted right-aligned triangle
+ * using the character `#`. If `size` is less than or equal to 0, it 
+ * prints only a new line.
+ * 
  * Return: Nothing.
  */
 void print_triangle(int size)
 {
-    int i, j;
+	int i, j;
 
-    if (size <= 0)
-    {
-        _putchar('\n');
-        return;
-    }
-    for (i = 1; i <= size; i++)
-    {
-        for (j = 1; j <= size - i; j++)
-        {
-            _putchar(' ');
-        }
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
 
-        for (j = 1; j <= i; j++)
-        {
-            _putchar('#');
-        }
-        _putchar('\n');
-    }
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+		for (j = size - i; j > 0; j--)
+		{
+			_putchar('#');
+		}
+
+		_putchar('\n');
+	}
 }
