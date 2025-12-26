6cf598e963a126abc0e140349aebc9852a5e5c38
niceehi <hsnislam2026@gmail.com>
2025-12-27 01:36:20 +0400

    Task 4

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -13,8 +13,8 @@ int _pow_recursion(int x, int y)
     {
         return(-1);
     }
-    else if(x == 0)
+    else if(y == 0)
         return(1);
-        
+
     return(x * _pow_recursion(x, y-1));
 }
