570a170463a0315735872995defcb5c2a75e1fa3
niceehi <hsnislam2026@gmail.com>
2025-12-28 21:46:44 +0400

    Task 3

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -2,34 +2,38 @@
 #include <stdlib.h>
 
 /**
- * alloc_grid - создает двумерный массив целых чисел (без очистки при ошибке)
- * @width: ширина
- * @height: высота
+ * alloc_grid - creates a 2D array of integers
+ * @width: width of the grid
+ * @height: height of the grid
  *
- * Return: указатель на массив или NULL
+ * Return: pointer to 2D array, or NULL on failure
  */
 int **alloc_grid(int width, int height)
 {
-    int **grid;
-    int i, j;
+	int **grid;
+	int i, j;
 
-    if (width <= 0 || height <= 0)
-        return (NULL);
+	if (width <= 0 || height <= 0)
+		return (NULL);
 
-    grid = malloc(sizeof(int *) * height);
-    if (grid == NULL)
-        return (NULL);
+	grid = malloc(sizeof(int *) * height);
+	if (grid == NULL)
+		return (NULL);
 
-    for (i = 0; i < height; i++)
-    {
-        grid[i] = malloc(sizeof(int) * width);
-        
-        if (grid[i] == NULL)
-            return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(sizeof(int) * width);
+		if (grid[i] == NULL)
+		{
+			for (i--; i >= 0; i--)
+				free(grid[i]);
+			free(grid);
+			return (NULL);
+		}
 
-        for (j = 0; j < width; j++)
-            grid[i][j] = 0;
-    }
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
+	}
 
-    return (grid);
+	return (grid);
 }
