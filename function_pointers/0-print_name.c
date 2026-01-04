dc440659c08b8a876efdbf43b3a101238e948daf
niceehi <hsnislam2026@gmail.com>
2026-01-04 23:36:32 +0400

    Task 0

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,11 +1,11 @@
 #include "function_pointers.h"
+
 /**
  *print_name - Print a name function
  *@name: name of the person
  *@f: function to print.
  *Return: void
  */
-
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && f != NULL)
