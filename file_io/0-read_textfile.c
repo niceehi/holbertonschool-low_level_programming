8af84eef0d6e8f53a46bc5a008740a67d7541535
niceehi <hsnislam2026@gmail.com>
2026-01-27 02:35:59 +0400

    Task 0

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: name of the file to read
+ * @letters: number of letters it should read and print
+ *
+ * Return: actual number of letters it could read and print
+ * 0 if filename is NULL
+ * 0 if write fails or does not write the expected amount of bytes
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t r_count, w_count;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+
+	r_count = read(fd, buffer, letters);
+	if (r_count == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	w_count = write(STDOUT_FILENO, buffer, r_count);
+
+	free(buffer);
+	close(fd);
+
+	if (w_count == -1 || w_count != r_count)
+		return (0);
+
+	return (w_count);
+}
