eb36e86221525cfc60c907deb77b4af3c8fc5f88
niceehi <hsnislam2026@gmail.com>
2026-01-27 19:23:54 +0400

    Task 2

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -22,7 +22,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
     if (text_content != NULL)
     {
-        while (text_content[len] != '\0')
+        while (text_content[len])
             len++;
 
         w_count = write(fd, text_content, len);
