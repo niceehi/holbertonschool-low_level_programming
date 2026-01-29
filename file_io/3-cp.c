66f2b12e82eb83b503339a233e0e22bf896e2e68
niceehi <hsnislam2026@gmail.com>
2026-01-29 14:50:08 +0400

    Task 3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -6,7 +6,7 @@
  * @arg: An array of pointers to the arguments
  * Return: 0 on success, or exits with 97, 98, 99, 100 on failure
  */
-int main(int argc, char *arg[]) 
+int main(int argc, char *argv[]) 
 {
     int fd, fd1; 
     ssize_t w_status;
@@ -19,17 +19,17 @@ int main(int argc, char *arg[])
         exit(97);
     }
 
-    fd = open(arg[1], O_RDONLY);
+    fd = open(argv[1], O_RDONLY);
     if (fd == -1)
     {
-        dprintf(2, "Error: Can't read from file %s\n", arg[1]);
+        dprintf(2, "Error: Can't read from file %s\n", argv[1]);
         exit(98);
     }
 
-    fd1 = open(arg[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+    fd1 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
     if (fd1 == -1)
     {
-        dprintf(2, "Error: Can't write to %s\n", arg[2]);
+        dprintf(2, "Error: Can't write to %s\n", argv[2]);
         close(fd);
         exit(99);
     }
@@ -40,7 +40,7 @@ int main(int argc, char *arg[])
         
         if (w_status != r_status)
         {
-            dprintf(2, "Error: Can't write to %s\n", arg[2]);
+            dprintf(2, "Error: Can't write to %s\n", argv[2]);
             close(fd);
             close(fd1);
             exit(99);
@@ -49,7 +49,7 @@ int main(int argc, char *arg[])
 
     if (r_status == -1)
     {
-        dprintf(2, "Error: Can't read from file %s\n", arg[1]);
+        dprintf(2, "Error: Can't read from file %s\n", argv[1]);
         exit(98);
     }
 
